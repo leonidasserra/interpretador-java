fb2821495e8ad850532900940c521e01f550aca8
Leonidas <leonidas.serra16@gmail.com>
2025-10-05 14:57:02 -0300

    fix: atualizando parser pra suportar o number

--- trabalho_compiladores/src/main/java/Parser.java
+++ trabalho_compiladores/src/main/java/Parser.java
@@ -1,62 +1,58 @@
-//
-//public class Parser {
-//
-//    private Scanner scan;
-//    private char currentToken;
-//
-//    public Parser(byte[] input) {
-//        scan = new Scanner(input);
-//        currentToken = scan.nextToken();
+
+public class Parser {
+
+    private Scanner scan;
+    private Token currentToken;
+
+    public Parser(byte[] input) {
+        scan = new Scanner(input);
+        currentToken = scan.nextToken();
+    }
+
+    private void nextToken () {
+        currentToken = scan.nextToken();
+    }
+
+
+    void expr() {
+        number();
+        oper();
+    }
+    void number () {
+        System.out.println("push " + currentToken.lexeme);
+        match(TokenType.NUMBER);
+    }
+
+
+    void oper () {
+        if (currentToken.type == TokenType.PLUS) {
+            match(TokenType.PLUS);
+            number();
+            System.out.println("add");
+            oper();
+        } else if (currentToken.type == TokenType.MINUS) {
+            match(TokenType.MINUS);
+            number();
+            System.out.println("sub");
+            oper();
+        }
+    }
+
+    private void match(TokenType t) {
+        if (currentToken.type == t) {
+            nextToken();
+        }else {
+            throw new Error("syntax error");
+        }
+    }
+
+    public void parse () {
+        expr();
+    }
+
+//    private char peek () {
+//        if (current < input.length)
+//            return (char)input[current];
+//        return '\0';
 //    }
-//
-//    private void nextToken () {
-//        currentToken = scan.nextToken();
-//    }
-//
-//
-//    void expr() {
-//        digit();
-//        oper();
-//    }
-//    void digit () {
-//        if (Character.isDigit(currentToken)) {
-//            System.out.println("push " + currentToken);
-//            match(currentToken);
-//        } else {
-//            throw new Error("syntax error");
-//        }
-//    }
-//
-//
-//    void oper () {
-//        if (currentToken == '+') {
-//            match('+');
-//            digit();
-//            System.out.println("add");
-//            oper();
-//        } else if (currentToken == '-') {
-//            match('-');
-//            digit();
-//            System.out.println("sub");
-//            oper();
-//        }
-//    }
-//
-//    private void match(char t) {
-//        if (currentToken == t) {
-//            nextToken();
-//        }else {
-//            throw new Error("syntax error");
-//        }
-//    }
-//
-//    public void parse () {
-//        expr();
-//    }
-//
-////    private char peek () {
-////        if (current < input.length)
-////            return (char)input[current];
-////        return '\0';
-////    }
-//}
+}
